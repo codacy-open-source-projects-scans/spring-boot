c6db3de545a92dba829976b23179359f8fe91f11
Codacy Archive <codacy-archive@local>
2026-03-12 01:20:39 +0000

    snapshot 2026-03-12T01:20:39.655700+00:00

--- module/spring-boot-micrometer-tracing/src/main/java/org/springframework/boot/micrometer/tracing/autoconfigure/otlp/package-info.java
+++ module/spring-boot-micrometer-tracing/src/main/java/org/springframework/boot/micrometer/tracing/autoconfigure/otlp/package-info.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012-present the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the License);
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,17 +14,10 @@
  * limitations under the License.
  */
 
-plugins {
-	id 'java'
-	id 'org.springframework.boot' version '{version}'
-}
+/**
+ * Auto-configuration for OTLP (OpenTelemetry Protocol) Exemplars with Micrometer Tracing.
+ */
+@NullMarked
+package org.springframework.boot.micrometer.tracing.autoconfigure.otlp;
 
-group = 'com.example'
-version = '1.0'
-
-springBoot {
-	buildInfo()
-	mainClass = "com.example.Main"
-}
-
-
+import org.jspecify.annotations.NullMarked;
